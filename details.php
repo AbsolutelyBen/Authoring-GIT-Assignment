b85a076767200a96a4f3d087c94078b66435b320
triciaFanson <triciafanson@gmail.com>
2016-04-01 19:15:13 -0400

    tricia's first commit
    # Conflicts:
    #	index.php

---
+++ details.php
@@ -0,0 +1,28 @@
+<!--<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Details Page</title>
+	<link href="css/foundation.css" rel="stylesheet" type="text/css">
+	<link href="css/foundation.min.css" rel="stylesheet" type="text/css">
+	<link href="css/normalize.css" rel="stylesheet" type="text/css">
+    <link href="css/main.css" rel="stylesheet" type="text/css" media="screen">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:700,300' rel='stylesheet' type='text/css'>
+
+</head>
+
+<body>
+
+	<?php /*include('includes/nav.html');*/ ?>
+
+	<div class="row">
+    	<div class="large-12 medium-12 small-12 columns details"></div><!--The empty <div> like the one shown in facebook that fills with the data from php and JSON-->
+    <!--</div>-->
+
+<!--<?php /*include('includes/footer.html');*/ ?>
+
+<!--<script src="js/utility.js"></script>
+<script src="js/foundation.js"></script>
+
+</body>
+</html>-->
